387fd0c48e2906ff3cd269d9f7a81467ab6233dd
Nedu Robert <nedumrobert@gmail.com>
2020-04-02 05:29:31 -0400

    fix: fixed naming issue

--- src/features/ChatWidget/views/ChatWidgetMain.tsx
+++ src/features/ChatWidget/views/ChatWidgetMain.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-import { IMessage } from '../containers/ChatWidget'
+import { Message } from '../containers/ChatWidget'
 import ChatWidgetCard from './ChatWidgetCard'
 import './styles.scss'
 
 interface Props {
-  messages: IMessage[]
+  messages: Message[]
 }
 
 const ChatWidgetMain: React.FC<Props> = ({ messages }: Props) => {
   return (
     <div className="main-container">
       <div className="scrolling-div">
-        {messages.map((message: IMessage, index: number) => (
+        {messages.map((message: Message, index: number) => (
           <ChatWidgetCard key={index} message={message} />
         ))}
       </div>
